12c91639ef65e75445faeca16cc9199c1d15f07f
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-06-09 14:43:01 -0500

    Few changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :trackable, :validatable
 
+  devise :timeoutable, :timeout_in => 20.minutes
+
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :dob, :presence => true
